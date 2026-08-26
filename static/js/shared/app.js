a8e3f7b02119b532703ae4e589b9b7115c565d0c
thanyeal <dan.mbpi@gmail.com>
2026-08-26 13:30:30 +0800

    update: delete nested folder for javascript files per module

--- static/js/shared/app.js
+++ static/js/shared/app.js
@@ -5,8 +5,8 @@ import { initModals } from './global/modal.js';
 import { initDropdowns, initCombobox } from './ui/dropdown.js';
 import { initAjaxForms } from './ui/forms.js';
 import { initScatterGraph } from './ui/scatter_graph.js';
-import { initSamplesRecordPage } from '../pages/samples_record.js';
-import { initSamplesReaderPage } from '../pages/samples_reader.js';
+import { initSamplesRecordPage } from './samples_record.js';
+import { initSamplesReaderPage } from './samples_reader.js';
 
 export function initApp() {
   initFooterClock();
